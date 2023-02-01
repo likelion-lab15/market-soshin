ab7a1cc68beaa0263bdd578b905a022b008722bb
hyunzsu <kkm5169@naver.com>
2023-02-01 21:18:02 +0900

    [Add] login.js 생성

--- client/js/login.js
+++ client/js/login.js
@@ -1,14 +1,14 @@
 const id = document.getElementById('loginId')
 const password = document.getElementById('loginPw')
-const login = document.querySelector('.loginComplete__login')
+const loginBtn = document.querySelector('.loginComplete__login')
 
-login.addEventListener('click', () => {
+loginBtn.addEventListener('click', () => {
 	if(id.value === 'hyunzsu' && password.value === '0000') {
-		// location.href('main.html')
-    console.log('cool');
+		location.href('main.html')
+    // console.log('cool');
 	}
 	else {
-		// alert('아이디, 비밀번호를 확인해주세요.')
-    console.log('no');
+		alert('아이디, 비밀번호를 확인해주세요.')
+    // console.log('no');
 	}
 })
