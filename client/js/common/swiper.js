3ba1ecd5d3c0f6b95730e3234cca3608cdc53d16
Sungchan CHo <kjyy07@naver.com>
2023-02-02 00:41:17 +0900

    [Add] 스와이퍼 추가

--- client/js/common/swiper.js
+++ client/js/common/swiper.js
@@ -1,15 +1,31 @@
-export let productsSwiper = new Swiper(".mySwiper", {
+export let productsSwiper1 = new Swiper(".mySwiper1", {
   slidesPerView: 4,
   spaceBetween: 30,
   slidesPerGroup: 4,
-  loop: true,
+  loop: false,
   loopFillGroupWithBlank: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".button1-next",
+    prevEl: ".button1-prev",
+  },
+});
+
+export let productsSwiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".button2-next",
+    prevEl: ".button2-prev",
   },
 });
