60927a172ccb9e537971b5af18f7a2f8728206ca
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-02 16:05:05 +0900

    [Edit] 메인배너 pagination 수정

--- client/js/common/swiper.js
+++ client/js/common/swiper.js
@@ -4,10 +4,6 @@ export let productsSwiper1 = new Swiper(".mySwiper1", {
   slidesPerGroup: 4,
   loop: false,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".button1-next",
     prevEl: ".button1-prev",
@@ -20,22 +16,19 @@ export let productsSwiper2 = new Swiper(".mySwiper2", {
   slidesPerGroup: 4,
   loop: false,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".button2-next",
     prevEl: ".button2-prev",
   },
 });
 
+// mainBanner
 export const mainBannerSwiper = new Swiper(".mySwiper", {
   autoplay: true,
   loop: true,
   speed: 1000,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".mainBanner__pagination",
     type: "fraction",
   },
   navigation: {
