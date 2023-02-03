dfb37883a5444e6672268c5770111a0c31866ca5
Sungchan CHo <kjyy07@naver.com>
2023-02-03 18:05:39 +0900

    [Edit] popUp css 수정 및 cartTemplate 추가

--- client/js/main.js
+++ client/js/main.js
@@ -4,7 +4,42 @@ import {
   products,
   mainProduct,
   mainBannerSwiper,
-  openModal,
-  closeModal,
-  putModal,
+  // openModal,
+  // closeModal,
+  // putModal,
+  addCart,
+  cartTemplateMaking,
+  insertCartModal,
 } from "./common/index.js";
+
+const productName = document.querySelector(".product__name");
+const productDiscountPrice = document.querySelector(".product__discountPrice");
+const productPrice = document.querySelector(".product__price");
+const popUp = document.querySelector(".popUp");
+
+cartTemplateMaking(productName, productDiscountPrice, productPrice);
+
+insertCartModal(popUp);
+
+const putBtn = popUp.querySelector("#putCart");
+const closeBtn = popUp.querySelector("#closePopup");
+const openButton = document.querySelectorAll("#open");
+
+function openModal() {
+  popUp.classList.remove("hidden");
+}
+
+function closeModal() {
+  popUp.classList.add("hidden");
+}
+
+function putModal() {
+  popUp.classList.add("hidden");
+}
+
+openButton.forEach(cartIcon => {
+  cartIcon.addEventListener("click", openModal);
+});
+
+putBtn.addEventListener("click", putModal);
+closeBtn.addEventListener("click", closeModal);
