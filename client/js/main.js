a6f0e263412c1eaeca1ad32956e6790e77cb309b
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-05 16:39:55 +0900

    비동기 fetch, async, await

--- client/js/main.js
+++ client/js/main.js
@@ -5,6 +5,50 @@
 
 const url = "http://localhost:3000/products";
 
+//fetch 방법
+// fetch(url)
+//   .then(response => response.json())
+//   .then(json => {
+//     return json.forEach(item => {
+//       console.log(item);
+//     });
+//   });
+
+// async, await + fetch
+async function init() {
+  const response = await fetch(url);
+  const jsonArray = await response.json();
+  //데이터 배열인 jsonArray 반환
+  return jsonArray;
+}
+
+init();
+
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", url);
+// xhr.send();
+
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4 && xhr.state === 200) {
+//     let productData = JSON.parse(xhr.responseText);
+//     renderHTML(productData);
+//   }
+// };
+
+// let aa = true;
+// const promise = new Promise((resolve, reject) => {
+//   if (aa) resolve("asd");
+//   else reject("as");
+// });
+
+// promise
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
 // const get = url => {
 //   return new Promise((resolve, reject) => {
 //     const xhr = new XMLHttpRequest();
