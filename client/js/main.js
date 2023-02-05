146cb4f104353a9a8a7a0c5c76910710769af276
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-05 12:34:24 +0900

    [Edit] 깃허브연동계정용 커밋

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,34 @@
 // import { productsSwiper2, products, mainProduct } from "./common/index.js";
 
 // import { mainBannerSwiper, mainTopSwiper } from "./swiper/index.js";
-import { getData } from "./common/index.js";
+// import { getData } from "./common/index.js";
 
-getData();
+const url = "http://localhost:3000/products";
+
+// const get = url => {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", url);
+//     xhr.send();
+//     xhr.onload = () => {
+//       if (xhr.status === 200) {
+//         resolve(JSON.parse(xhr.response));
+//       } else {
+//         reject("실패!");
+//       }
+//     };
+//   });
+// };
+
+// get(url).then(res => {
+//   return res;
+// });
+
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", url);
+// xhr.send();
+// xhr.onreadystatechange = () => {
+//   if(xhr.readyState === 4 && xhr/state === 200) {
+
+//   }
+// }
