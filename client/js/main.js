b370dc8d88c69d5add37cd0b7460ec6a407b8557
kimzeze <fridaynight@kakao.com>
2023-02-07 10:09:26 +0900

    [Add] 상품 선택 JS 구현, 테스트 코드 마크업 수정

--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,9 @@
 // 아코디언 JS 기능 코드 시작
 
 const storageType = document.querySelectorAll(".accordionButton");
-const productList = document.querySelector(".productInput");
 const productCold = document.querySelector(".cold");
 const productFreeze = document.querySelector(".freeze");
 const productStandard = document.querySelector(".standard");
-
-console.log(storageType);
-console.log(productList);
 
 storageType.forEach(function (storage) {
   storage.addEventListener("click", function () {
@@ -25,28 +21,96 @@ storageType.forEach(function (storage) {
 
 // 아코디언 JS 기능 코드 끝
 
-/*     productList.forEach(item => {
-      if (item.classList.contains("cold")) {
-        console.log("1");
-        productCold.classList.toggle("active");
-      } else if (item.classList.contains("freeze")) {
-        console.log("2");
-        productFreeze.classList.toggle("active");
-      } else {
-        console.log("3");
-        productStandard.classList.toggle("active");
-      } */
+// 전체선택 JS 구현 시작
 
-// if (storage.classList.contains("active")) {
-//   productList.forEach(item => {
-//     item.classList.toggle("active");
+//
+
+const selectCount = document.querySelectorAll('input[id="select"]'); // 전체 체크박스 개수
+const checked = document.querySelectorAll('input[id="select"]:checked'); // 선택된 체크박스
+// const checkAll = document.querySelector('input[id="checkAll"]'); // check all 체크박스
+
+// console.log(selectCount);
+// console.log(checked);
+// console.log(selectCount.length);
+
+const cartBody = document.querySelector(".cartList__body");
+console.log(cartBody);
+cartBody.addEventListener("click", e => {
+  if (e.target.tagName === "LABEL") {
+    if (e.target.checked === false) {
+      e.target.style.backgroundImage =
+        "url('../assets/icons/isChecked=true.svg')";
+      e.target.checked = true;
+    } else {
+      e.target.style.backgroundImage =
+        "url('../assets/icons/isChecked=false.svg')";
+      e.target.checked = false;
+    }
+  }
+
+  /*   if (e.target.tagName === "LABEL" && e.target.checked === false) {
+    console.log(e.target.checked);
+    e.target.style.backgroundImage =
+      "url('../assets/icons/isChecked=true.svg')";
+    e.target.checked = true;
+  } else if (e.target.tagName === "LABEL" && e.target.checked === true) {
+    console.log(e.target.checked);
+    e.target.style.backgroundImage =
+      "url('../assets/icons/isChecked=false.svg')";
+    e.target.checked = false;
+  } */
+});
+
+// cartBody.addEventListener("click", e => {
+//   if (e.target.checked === true) {
+//     e.target.style.backgroundImage =
+//       "url('../assets/icons/isChecked=false.svg')";
+//     e.target.checked = false;
+//     console.log(e.target.checked === true);
+//   }
+// });
+
+// list.addEventListener("click", (e) => {
+//   if (e.target.tagName === "LI") {
+//     console.log(e.target.innerText);
+//   }
+
+// console.log(checkAll);
+
+// for (let i = 0; i < selectCount.length; i++) {
+//   console.log(selectCount[i]);
+//   selectCount[i].addEventListener("click", function () {
+//     const label = this.closest("label");
+//     if (this.checked === true) {
+//       console.log(label);
+//       console.log(label.style.backgroundImage);
+//       label.style.backgroundImage = "url('../assets/icons/isChecked=true.svg')";
+//     } else if (this.checked === false) {
+//       label.style.backgroundImage =
+//         "url('../assets/icons/isChecked=false.svg')";
+//     }
+
+//     // console.log(selectCount[i].checked === true);
+//     //   item[i].checked = true;
+//     //   console.log(checked);
+//     //   // checked.forEach(checked => {
+//     //   //   console.log(checked);
+//     //   // });
 //   });
+// }
 
-/* if (e.target === productCold) {
-  productCold.classList.toggle("active");
-}
-if (e.target === productFreeze) {
-  productFreeze.classList.toggle("active");
-}
-if (e.target === productStandard) {
-  productStandard.classList.toggle("active"); */
+// selectCount.addEventListener("click", function () {
+//   console.log("click");
+//   if (selectCount.length === checked.length) {
+//     checkAll.checked = true;
+//   } else {
+//     checkAll.checked = false;
+//   }
+// });
+
+// function selectAll(selectAll) {
+//   const selectCount = document.getElementsByName("select");
+//   selectCount.forEach(checkbox => {
+//     checkbox.checked = selectAll.checked;
+//   });
+// }
