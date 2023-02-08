29f64f76f9c97ec7546f4456025faeb28f867956
hyunzsu <kkm5169@naver.com>
2023-02-08 16:09:15 +0900

    [Add] 버튼 js 기능 완성

--- client/js/common/button.js
+++ client/js/common/button.js
@@ -1,59 +1,41 @@
-/* import { attr } from '../../lib/dom/attr.js';
-import { getNodes, getNode } from '../../lib/dom/getNode.js';
-import { addClass, removeClass } from '../../lib/dom/css.js';
 
+const cartButton = document.querySelectorAll(".cartList__product");
+const minus = document.querySelectorAll('.quantity__minus')
+const result = document.querySelectorAll('.quantity__result')
+const plus = document.querySelectorAll('.quantity__plus')
 
-const minus = getNodes('.quantity__minus')
-const result = getNodes('.quantity__result')
-const plus = getNodes('.quantity__plus')
+let resultNumberArr = [];
+// let resultNumber = 1;
 
-
-let i = 1;
-export function plusHandler() {
-  i++
-  result.textContent = i;
-  minus.disabled = false;
-  minus.addClass('quantity__minus--change')
+export function plusHandler(num) {
+  resultNumberArr[num]++;
+  result[num].textContent = resultNumberArr[num];
+  minus[num].disabled = false;
+  minus[num].classList.add('quantity__minus--change')
 }
 
-export function minusHandler() {
-  if (i > 1) {
-    i--
-    result.textContent = i;
-    if (i == 1) {
-      arrminus.removeClass('quantity__minus--change')
+export function minusHandler(num) {
+  if (resultNumberArr[num] > 1) {
+    resultNumberArr[num]--;
+    result[num].textContent = resultNumberArr[num];
+    if (resultNumberArr[num] == 1) {
+      minus[num].classList.remove('quantity__minus--change')
     }
   } else {
-    minus.addClass('quantity__minus')
-    minus.removeClass('quantity__minus--change')
+    minus[num].classList.add('quantity__minus')
+    minus[num].classList.remove('quantity__minus--change')
   }
 }
 
-plus.addEventListener('click', plusHandler)
-plus.addEventListener('click', minusHandler) */
-
-/* [].forEach.call(plus,function(plus) {
-  plus.addEventListener('click', plusHandler)
-}) */
-
-
-/* [].forEach.call(minus,function(minus) {
-  minus.addEventListener('click', minusHandler)
-}); */
-
-
-const cartBody = document.querySelector(".cartList__body");
-console.log(cartBody);
-
-/* cartBody.addEventListener("click", e => {
-  if (e.target.tagName === "LABEL") {
-    if (e.target.checked === false) {
-      e.target.style.backgroundImage =
-        "url('../assets/icons/isChecked=true.svg')";
-      e.target.checked = true;
-    } else {
-      e.target.style.backgroundImage =
-        "url('../assets/icons/isChecked=false.svg')";
-      e.target.checked = false;
-    }
-  } */
+for(let i = 0; i < cartButton.length; i++) {
+  resultNumberArr.push(0);
+  cartButton[i].addEventListener('click', (e) => {
+    console.log(resultNumberArr);
+    let target = e.target;
+    if(target === plus[i]) {
+      plusHandler(i);
+    } else if(target === minus[i]) {
+      minusHandler(i);
+    } 
+  });
+}
