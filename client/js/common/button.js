a4541289c13556af4c1ced8840352f09d7b8aa97
kimzeze <fridaynight@kakao.com>
2023-02-08 16:46:09 +0900

    [Add] 선택버튼 JS 활성화, 장바구니 h2 추가

--- client/js/common/button.js
+++ client/js/common/button.js
@@ -1,8 +1,7 @@
-
 const cartButton = document.querySelectorAll(".cartList__product");
-const minus = document.querySelectorAll('.quantity__minus')
-const result = document.querySelectorAll('.quantity__result')
-const plus = document.querySelectorAll('.quantity__plus')
+const minus = document.querySelectorAll(".quantity__minus");
+const result = document.querySelectorAll(".quantity__result");
+const plus = document.querySelectorAll(".quantity__plus");
 
 let resultNumberArr = [];
 // let resultNumber = 1;
@@ -11,7 +10,7 @@ export function plusHandler(num) {
   resultNumberArr[num]++;
   result[num].textContent = resultNumberArr[num];
   minus[num].disabled = false;
-  minus[num].classList.add('quantity__minus--change')
+  minus[num].classList.add("quantity__minus--change");
 }
 
 export function minusHandler(num) {
@@ -19,23 +18,23 @@ export function minusHandler(num) {
     resultNumberArr[num]--;
     result[num].textContent = resultNumberArr[num];
     if (resultNumberArr[num] == 1) {
-      minus[num].classList.remove('quantity__minus--change')
+      minus[num].classList.remove("quantity__minus--change");
     }
   } else {
-    minus[num].classList.add('quantity__minus')
-    minus[num].classList.remove('quantity__minus--change')
+    minus[num].classList.add("quantity__minus");
+    minus[num].classList.remove("quantity__minus--change");
   }
 }
 
-for(let i = 0; i < cartButton.length; i++) {
+for (let i = 0; i < cartButton.length; i++) {
   resultNumberArr.push(0);
-  cartButton[i].addEventListener('click', (e) => {
-    console.log(resultNumberArr);
+  cartButton[i].addEventListener("click", e => {
+    // console.log(resultNumberArr);
     let target = e.target;
-    if(target === plus[i]) {
+    if (target === plus[i]) {
       plusHandler(i);
-    } else if(target === minus[i]) {
+    } else if (target === minus[i]) {
       minusHandler(i);
-    } 
+    }
   });
 }
