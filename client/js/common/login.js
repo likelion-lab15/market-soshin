970be43ffddc8cd399be6baa147f220452d23774
hyunzsu <kkm5169@naver.com>
2023-02-08 09:29:53 +0900

    [Add] 로그인 js 기능 구현

--- client/js/common/login.js
+++ client/js/common/login.js
@@ -1,36 +1,21 @@
-/* const id = document.getElementById('loginId')
+
+
+const id = document.getElementById('loginId')
 const password = document.getElementById('loginPw')
 const loginBtn = document.querySelector('.loginComplete__login')
-
-loginBtn.addEventListener('click', () => {
-	if(id.value === 'hyunzsu' && password.value === '0000') {
-		location.href('main.html')
-	}
-	else {
-		// alert('아이디, 비밀번호를 확인해주세요.')
-		const openButton = document.querySelector(".loginComplete__login");
-		const popUp = document.querySelector(".popUp");
-		const putBtn = popUp.querySelector("#putCart");
-		const closeBtn = popUp.querySelector("#closePopup");
-		console.log(openButton);
-		console.log(popUp, putBtn, closeBtn);
-
-		const openModal = () => {
-			popUp.classList.remove("hidden");
-		};
-		const closeModal = () => {
-			popUp.classList.add("hidden");
-		};
-		const putModal = () => {
-			popUp.classList.add("hidden");
-		};
-		putBtn.addEventListener("click", putModal);
-		closeBtn.addEventListener("click", closeModal);
-		openButton.addEventListener("click", openModal);
-	}
-}) */
-
+const checkBtn = document.querySelector('.popUp__product--button')
 const popUp = document.querySelector(".popUp");
+
+// 로컬스토리지 테스트
+const user = { id: "hjs", password: '0000'} // 로컬스토리지 넣을 임의의 값
+
+localStorage.setItem("H", JSON.stringify(user)) 
+console.log(localStorage.getItem("H"));
+let K = localStorage.getItem("H")
+
+console.log(JSON.parse(K));
+let Y = JSON.parse(K)
+console.log(Y.id);
 
 export const openModal = () => {
 	popUp.classList.remove("hidden");
@@ -39,3 +24,16 @@ export const openModal = () => {
 export const closeModal = () => {
   popUp.classList.add("hidden");
 };
+
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+	if(id.value === Y.id && password.value === Y.password) { // 로컬스토리지 값이 맞으면 로그인 성공
+    location.replace('../hello.html')
+	}
+	else {
+    openModal();
+	}
+})
+
+checkBtn.addEventListener('click', closeModal)
--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,2 @@
 
-import { closeModal, openModal } from "./common/login.js";
-
-const id = document.getElementById('loginId')
-const password = document.getElementById('loginPw')
-const loginBtn = document.querySelector('.loginComplete__login')
-const checkBtn = document.querySelector('.popUp__product--button')
-
-loginBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-	if(id.value === 'hyunzsu' && password.value === '0000') {
-    location.replace('../index.html')
-	}
-	else {
-    const popUp = document.querySelector(".popUp");
-    console.log(popUp);
-    openModal();
-	}
-})
-
-checkBtn.addEventListener('click', closeModal)
+import { openModal, closeModal } from "./common/index.js";
