6a2f71250b0c71c24b461a89a5c2ac789b8cb081
김진우 <rlawlsdn263@naver.com>
2023-02-09 10:53:49 +0900

    [Edit] 불필요한 페이지 삭제

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,1 @@
 import { mainBannerSwiper, mainTopSwiper, mainBottomSwiper } from "./swiper/index.js";
-// import { mainBannerSwiper, mainTopSwiper, mainBottomSwiper } from '../js/swiper/index.js';
-
-//메인 페이지 상단, 하단 스와이퍼 동적추가
-// displayItemHome();
